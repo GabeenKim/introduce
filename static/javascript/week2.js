27a632b5098b9bde6b50cd44e2f80a29d965bec3
GabeenKim <rlarkqls5551250@gmail.com>
2022-04-30 11:38:09 +0900

    Merge branch 'main' of https://github.com/GabeenKim/introduce

--- static/javascript/week2.js
+++ static/javascript/week2.js
@@ -1,6 +1,8 @@
+let btn = document.getElementById('day');
+
 function change(){
-    var section = document.querySelectorAll('.about-box, .img-box, .about-me-text, .about-me-title')
-    var section2 =  document.querySelectorAll('.main-box, .sub-box, .sub-box2, .box2-title')
+    let section = document.querySelectorAll('.about-box, .img-box, .about-me-text, .about-me-title')
+    let section2 =  document.querySelectorAll('.main-box, .sub-box, .sub-box2, .box2-title')
     
     if(btn.value=="Day"){
         btn.value= "Night";
@@ -8,13 +10,13 @@ function change(){
         document.querySelector('body').style.backgroundColor='#282828';
         document.querySelector('body').style.color='white';
 
-        for (var i = 0; i < section.length; i++) {
-            var item = section.item(i);
+        for (let i = 0; i < section.length; i++) {
+            let item = section.item(i);
             item.style.color = "white";
           }
 
-        for (var i = 0; i < section2.length; i++) {
-        var item = section2.item(i);
+        for (let i = 0; i < section2.length; i++) {
+        let item = section2.item(i);
         item.style.color = "white";
         } 
     }
@@ -24,16 +26,21 @@ function change(){
         document.querySelector('body').style.backgroundColor='white';
         document.querySelector('body').style.color='black';  
 
-        for (var i = 0; i < section.length; i++) {
-            var item = section.item(i);
+        for (let i = 0; i < section.length; i++) {
+            let item = section.item(i);
             item.style.color = "black";
           }
-        for (var i = 0; i < section2.length; i++) {
-        var item = section2.item(i);
+        for (let i = 0; i < section2.length; i++) {
+        let item = section2.item(i);
         item.style.color = "black";
         }   
     }
 }
+
+const content = "안녕하세요 김가빈입니다!"
+const text = document.getElementById("dynamic");
+let index=0;
+let txt="";
 
 function typing(){
     if(index < content.length){
@@ -43,23 +50,11 @@ function typing(){
     }
 }
 
-var btn = document.getElementById('day');
-
-const content = "안녕하세요 김가빈입니다!"
-const text = document.getElementById("dynamic");
-let index=0;
-let txt="";
-
-setInterval(typing,120);
-
-
-scrollTop('scrollButton', 500);
-
 function scrollTop(elem_id,duration) {
 	let target = document.getElementById(elem_id);
 
 	target.addEventListener('click', function() {
-		let currentY = window.pageYOffset; 
+		let currentY = window.pageYOffset;                 //웹페이지에서 스크롤정보 받아오기
         let step;
         if ( duration/currentY > 1 ){
             step = 10;
@@ -80,3 +75,12 @@ function scrollTop(elem_id,duration) {
 		}
 	});
 }
+
+
+
+
+
+setInterval(typing,120);
+
+scrollTop('scrollButton', 500);
+
